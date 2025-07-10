59eb49650047ddc4805c9ceb4be4954b302c1672
Andres Garcia <andres.garcia@databricks.com>
2025-07-10 16:40:08 -0600

    Refactor cli command for Analyze.

--- tests/unit/test_cli_analyze.py
+++ tests/unit/test_cli_analyze.py
@@ -1,33 +1,29 @@
 from pathlib import Path
-from unittest.mock import create_autospec, patch
 
 from databricks.labs.blueprint.tui import MockPrompts
 from databricks.labs.lakebridge import cli
-from databricks.sdk import WorkspaceClient
-
-from databricks.labs.lakebridge.contexts.application import ApplicationContext
 
 
 from databricks.labs.bladespector.analyzer import Analyzer
 
 
-def test_analyze():
-    supported_tech = sorted(Analyzer.supported_source_technologies(), key=str.casefold)
-    numbered_list = dict(enumerate(supported_tech))
-    tech_enum = next((key for key, value in numbered_list.items() if value == "Informatica - PC"), 12)
+INPUT_PATH = Path(__file__).parent.parent / "resources" / "functional" / "informatica"
+SUPPORTED_TECH = sorted(Analyzer.supported_source_technologies(), key=str.casefold)
+TECH_ENUM = next((i for i, tech in enumerate(SUPPORTED_TECH) if tech == "Informatica - PC"), 12)
 
+
+def test_analyze(mock_workspace_client):
     prompts = MockPrompts(
         {
-            r"Select the source technology": tech_enum,
+            "Select the source technology": TECH_ENUM,
+            "Enter full path to the source directory": INPUT_PATH,
+            "Enter report filename": "",
         }
     )
-    with patch.object(ApplicationContext, "prompts", prompts):
-        ws = create_autospec(WorkspaceClient)
-        input_path = str(Path(__file__).parent.parent / "resources" / "functional" / "informatica")
-        cli.analyze(ws, input_path, "/tmp/sample.xlsx")
+    cli.analyze(mock_workspace_client, prompts)
 
-
-def test_analyze_source_override():
-    ws = create_autospec(WorkspaceClient)
-    input_path = str(Path(__file__).parent.parent / "resources" / "functional" / "informatica")
-    cli.analyze(ws, input_path, "/tmp/sample.xlsx", "Informatica - PC")
+    expected_filename = "lakebridge-analyzer-results.xlsx"
+    created_file = list(INPUT_PATH.glob(expected_filename))
+    assert any(f.name == expected_filename for f in created_file)
+    if created_file:
+        created_file[0].unlink()
