0a825558c0950aaefd7d557a8d27a7f60f824214
Andres Garcia <andres.garcia@databricks.com>
2025-07-11 18:01:56 -0600

    feat(analyzer): improve argument handling and validation

--- tests/unit/test_cli_analyze.py
+++ tests/unit/test_cli_analyze.py
@@ -16,13 +16,13 @@ def test_analyze(mock_workspace_client, tmp_path: Path):
     tech_enum = next((i for i, tech in enumerate(supported_tech) if tech == "Informatica - PC"), 12)
 
     source_dir = Path(__file__).parent.parent / "resources" / "functional" / "informatica"
-    output_dir = tmp_path / "results.xlsx"
+    output_dir = tmp_path / "results"
 
     mock_prompts = MockPrompts(
         {
             "Select the source technology": str(tech_enum),
             "Enter full path to the source directory": str(source_dir),
-            "Enter report filename": str(output_dir),
+            "Enter report file name or custom export path including file name without extension": str(output_dir),
         }
     )
     with patch.object(ApplicationContext, "prompts", mock_prompts):
