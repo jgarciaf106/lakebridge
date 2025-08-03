08c2736a0d8ec028af788b935ba333970955f0cd
andresgarciaf <jgarciaf106@gmail.com>
2025-08-03 12:39:02 -0600

    propose changes to handle onedrive

--- src/databricks/labs/lakebridge/analyzer/lakebridge_analyzer.py
+++ src/databricks/labs/lakebridge/analyzer/lakebridge_analyzer.py
@@ -1,10 +1,11 @@
-import os
+import tempfile
 from pathlib import Path
 
 from databricks.labs.blueprint.entrypoint import get_logger
 from databricks.labs.blueprint.tui import Prompts
 
 from databricks.labs.bladespector.analyzer import Analyzer, _PLATFORM_TO_SOURCE_TECHNOLOGY
+from databricks.labs.lakebridge.helpers.file_utils import check_path, move_tmp_file
 
 logger = get_logger(__file__)
 
@@ -15,28 +16,12 @@ class LakebridgeAnalyzer(Analyzer):
         self._is_debug = is_debug
         super().__init__()
 
-    @staticmethod
-    def _val_path(path: str) -> bool:
-        """Validates a path for both existing files and writable files."""
-        try:
-            path_obj = Path(path) if not isinstance(path, Path) else path
-
-            if path_obj.exists():
-                return os.access(path_obj, os.W_OK)
-
-            parent = path_obj.parent
-            return parent.exists() and os.access(parent, os.W_OK)
-
-        except (OSError, ValueError):
-            logger.warning("Could not validate path: %s", path)
-            return False
-
     def _get_source_directory(self) -> Path:
         """Get and validate the source directory from user input."""
         directory_str = self._prompts.question(
             "Enter full path to the source directory",
             default=Path.cwd().as_posix(),
-            validate=self._val_path,
+            validate=check_path,
         )
         return Path(directory_str).resolve()
 
@@ -45,25 +30,34 @@ class LakebridgeAnalyzer(Analyzer):
         filename = self._prompts.question(
             "Enter report file name or custom export path including file name without extension",
             default=f"{directory.as_posix()}/lakebridge-analyzer-results.xlsx",
-            validate=self._val_path,
+            validate=check_path,
         )
-        return (
-            directory / Path(filename).with_suffix(".xlsx")
-            if len(filename.split("/")) == 1
-            else Path(filename).with_suffix(".xlsx")
-        )
+        return directory / Path(filename) if len(filename.split("/")) == 1 else Path(filename)
+
+    def _get_source_tech(self, platform: str = None) -> str:
+        """Validate source technology or prompt for a valid source"""
+        if platform is None or platform not in self.supported_source_technologies():
+            if platform is not None:
+                logger.warning(f"Invalid source technology {platform}")
+            platform = self._prompts.choice("Select the source technology", self.supported_source_technologies())
+        return _PLATFORM_TO_SOURCE_TECHNOLOGY.get(platform)
+
+    @staticmethod
+    def _temp_xlsx_path(results_dir: Path | str) -> Path:
+        return (Path(tempfile.mkdtemp()) / Path(results_dir).name).with_suffix(".xlsx")
 
     def _run_prompt_analyzer(self):
         """Run the analyzer: prompt guided"""
-        directory = self._get_source_directory()
-        result = self._get_result_file_path(directory)
+        source_dir = self._get_source_directory()
+        results_dir = self._get_result_file_path(source_dir)
+        tmp_dir = self._temp_xlsx_path(results_dir)
+        technology = self._get_source_tech()
 
-        platform = self._prompts.choice("Select the source technology", self.supported_source_technologies())
-        technology = _PLATFORM_TO_SOURCE_TECHNOLOGY.get(platform)
+        self._run_binary(source_dir, tmp_dir, technology, self._is_debug)
 
-        self._run_binary(directory, result, technology, self._is_debug)
+        move_tmp_file(tmp_dir, results_dir)
 
-        logger.info(f"Successfully Analyzed files in ${directory} for ${technology} and saved report to {result}")
+        logger.info(f"Successfully Analyzed files in ${source_dir} for ${technology} and saved report to {results_dir}")
 
     def _run_arg_analyzer(self, source_dir: str | None, results_dir: str | None, technology: str | None):
         """Run the analyzer: arg guided"""
@@ -71,12 +65,16 @@ class LakebridgeAnalyzer(Analyzer):
             logger.error("All arguments (--source-directory, --report-file, --source-tech) must be provided")
             return
 
-        if technology not in Analyzer.supported_source_technologies():
-            logger.warning(f"Invalida source technology {technology}")
-            technology = self._prompts.choice("Select the source technology", Analyzer.supported_source_technologies())
+        if check_path(source_dir) and check_path(results_dir):
+            tmp_dir = self._temp_xlsx_path(results_dir)
+            technology = self._get_source_tech(technology)
+            self._run_binary(Path(source_dir), tmp_dir, technology, self._is_debug)
 
-        if self._val_path(source_dir) and self._val_path(results_dir):
-            self.analyze(Path(source_dir), Path(results_dir).with_suffix(".xlsx"), technology, self._is_debug)
+            move_tmp_file(tmp_dir, Path(results_dir))
+
+            logger.info(
+                f"Successfully Analyzed files in ${source_dir} for ${technology} and saved report to {results_dir}"
+            )
 
     def run_analyzer(
         self, source_dir: str | None = None, results_dir: str | None = None, technology: str | None = None
